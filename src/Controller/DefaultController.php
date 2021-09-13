37ff14aec90c79b9651c9084dfad3f20bcccf8aa
Pierre Bretéché <pbreteche@dawan.fr>
2021-09-13 14:22:56 +0200

    Valeur par défaut de paramètre de route

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,17 +7,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends AbstractController
 {
-    public function index(): Response
-    {
-        return new Response(
-            'Bonjour, mon application Symfony fonctionne',
-            Response::HTTP_OK,
-            [
-                'Content-type' => 'text/plain',
-            ]
-        );
-    }
-
     public function hello(string $name): Response
     {
         return new Response('<h1>Bonjour '.$name.'!</h1>');
