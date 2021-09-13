237520641aa45f737ac4790718e55ce68d492b2d
Pierre Bretéché <pbreteche@dawan.fr>
2021-09-13 14:07:16 +0200

    Première méthode de contrôleur

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends AbstractController
+{
+    public function index(): Response
+    {
+        return new Response(
+            'Bonjour, mon application Symfony fonctionne',
+            Response::HTTP_OK,
+            [
+                'Content-type' => 'text/plain',
+            ]
+        );
+    }
+}
