aab8f094645bd0aed5671d57c23e6d3e47b8f767
Pierre Bretéché <pbreteche@dawan.fr>
2021-09-17 10:36:41 +0200

    Rappel: état d'un objet/service

---
+++ src/Demo/Calculator.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Demo;
+
+class Calculator
+{
+    private $inc = 0;
+
+    public function add(int $a, int $b)
+    {
+        // Ici, c'est stateless
+        // le résultat est toujours le même,
+        // car dépend uniquement des paramètres (fonction pure)
+        return $a + $b;
+    }
+
+    public function inc()
+    {
+        // Ici, on a un état. Un appel d'un utilisateur de cette méthode
+        // fera changer le résultat pour un autre utilisateur de la méthode
+        return $this->inc++;
+    }
+}
